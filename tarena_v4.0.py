eb12b7515d0d256abdeab3a9c50b8db53030f268
Master-Sun <779625568@qq.com>
2019-05-05 19:35:31 +0800

    完成v4.0,待实际测试

--- tarena_v4.0.py
+++ tarena_v4.0.py
@@ -1,8 +1,12 @@
 import requests, re, time, sys, os
 from Crypto.Cipher import AES
+from multiprocessing import Queue
+from threading import Thread
 
 # 待优化：分两次匹配了视频和key的url
 # 效率太低，如何启用多线程或多进程呢？？？
+# 思路：一个线程爬链接，3个线程负责下载
+# 待完成：拼接cmd命令，重复下载线程
 
 class Tarena_spider(object):
     def __init__(self):
@@ -22,14 +26,30 @@ class Tarena_spider(object):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
                 }
         self.root = os.getcwd()
-        self.download_path = self.root + '/cache'
+        self.down_list = Queue()
+        self.tlist = []
 
-    # 获取视频的url和key
+    # 创建线程爬取url和下载
     def run(self):
+        th1 = Thread(target=self.get_url)
+        th1.start()
+        self.tlist.append(th1)
+        # 多线程下载
+        for i in range(3):
+            th = Thread(target=self.download,args=(i,))
+            th.start()
+            self.tlist.append(th)
+        print('--------------所有线程创建完毕，等待回收...')
+        for th in self.tlist:
+            th.join()
+        print('所有线程已回收，爬取结束!')
+
+
+    def get_url(self):
         success_count = 0
         i = 0
         while success_count < self.course_num:
-            print('开始爬取第%d天视频'%(success_count+1))
+            print('开始爬取第%d天url'%(success_count+1))
             self.course_date += i
             course_no = self.course_name + str(self.course_date)
             url_am = 'http://videotts.it211.com.cn/' + course_no + 'am/' + course_no + 'am.m3u8'
@@ -51,9 +71,9 @@ class Tarena_spider(object):
             else:
                 success_count += 1
                 i = 1
-                print('第%d天视频爬取完成'%success_count)
-                time.sleep(5)
-        print('爬取结束')
+                print('第%d天url爬取完成'%success_count)
+                time.sleep(0.2)
+        print('----------url爬取线程结束')
 
 
     def handle(self,res,index,course_no):
@@ -62,53 +82,67 @@ class Tarena_spider(object):
         # 匹配ts文件地址
         p_url = re.compile(r'http://.*?\.ts')
         ts_list = p_url.findall(html)
+        for url in ts_list:
+            self.down_list.put(url)
         # 匹配密钥
         p_key = re.compile(r'http://.*?\.key')
         key_url = p_key.findall(html)[0]
         key_res = requests.get(url=key_url,headers=self.headers)
         # 返回二进制对象
         key = key_res.content
-        self.download(key,ts_list,index)
-        self.merge_file(index,course_no)
+        t = (key,ts_list,index,course_no)
+        self.down_list.put(t)
 
 
     # 下载url并解密
-    def download(self,key,ts_list,index):
-        if not os.path.exists(self.download_path):
-            os.mkdir(self.download_path)
+    def download(self,i):
+        while True:
+            try:
+                t = self.down_list.get(block=True,timeout=2)
+            except:
+                print('线程%d爬取结束'%(i+1))
+                break
+            key = t[0]
+            ts_list = t[1]
+            index = t[2]
+            course_no = t[3]
+            download_path = self.root + '\\cache' + str(i)
+            if not os.path.exists(download_path):
+                os.mkdir(download_path)
 
-        # 循环下载ts链接
-        count = 0
-        cryptor = AES.new(key, AES.MODE_CBC, key)
-        err_count = 0
-        for link in ts_list:
-            res = requests.get(url=link,headers=self.headers)
-            filename = self.download_path + '/' + '%03d'%count + '.mp4'
-            with open(filename,'wb') as f:
-                try:
-                    f.write(cryptor.decrypt(res.content))
-                except:
-                    print('-----------第%d个视频解密出错'%count)
-                    err_count += 1
-            count += 1
-            print('已爬完第%d个视频'%count)
-            time.sleep(0.2)
-        print('***************解密出错次数:%d'%err_count)
+            # 循环下载ts链接
+            count = 0
+            cryptor = AES.new(key, AES.MODE_CBC, key)
+            err_count = 0
+            for link in ts_list:
+                res = requests.get(url=link,headers=self.headers)
+                filename = download_path + '\\' + '%03d'%count + '.mp4'
+                with open(filename,'wb') as f:
+                    try:
+                        f.write(cryptor.decrypt(res.content))
+                    except:
+                        print('-----------第%d个视频解密出错'%count)
+                        err_count += 1
+                count += 1
+                print('下载线程%d:已爬完第%d个视频'%(i,count))
+                time.sleep(0.2)
+            print('***************解密出错次数:%d'%err_count)
+            self.merge_file(index,course_no,download_path)
         
 
     # 合成视频文件
-    def merge_file(self,index,course_no):
+    def merge_file(self,index,course_no,download_path):
         if not os.path.exists(self.folder):
             os.mkdir(self.folder)
-        os.chdir(self.download_path)
-        cmd = "copy /b * new.tmp"
+        src = download_path + '\\*.mp4'
+        dst = download_path + '\\new.tmp'
+        cmd = "copy /b " + src + ' ' + dst
         os.system(cmd)
-        os.system('del *.mp4')
+        os.system('del ' + src)
         if index == 0:
-            os.rename("new.tmp", self.folder + '/' + course_no + "am.mp4")
+            os.rename(dst, self.folder + '/' + course_no + "am.mp4")
         else:
-            os.rename("new.tmp", self.folder + '/' + course_no + "pm.mp4")
-        os.chdir(self.root)
+            os.rename(dst, self.folder + '/' + course_no + "pm.mp4")
 
 
 if __name__ == '__main__':
